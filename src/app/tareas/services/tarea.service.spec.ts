8957661fcd1c723d3c3efb63a27d98bfe3277abd
unknown <andresfelipehc@ufps.edu.co>
2022-09-10 16:47:37 -0500

    pruebas al servicio sobre borrar tareas

--- src/app/tareas/services/tarea.service.spec.ts
+++ src/app/tareas/services/tarea.service.spec.ts
@@ -10,4 +10,15 @@ describe('TraeaService', () => {
     it('ToBe: Debe verificar que mi servicio inicie con 4 tareas', ()=>{
         expect(service.tareas.length).toBe(4);
     })
+
+    it('Delete: deberia borrar una tarea', ()=>{
+
+        service.deleteTreas("Barrer");
+        expect(service.tareas.length).toBe(3);
+    })
+
+    it('No delete: No deberia borrar una tarea, porque no existe', ()=>{
+        service.deleteTreas("Cantar");
+        expect(service.tareas.length).toBe(4);
+    })
 })
